28479360fedbe6d706db429a306164f092b0cfec
espindolasol95 <espindolasol.ok@gmail.com>
2025-08-25 23:01:36 +0200

    feat(preparazionede di header main e fotter in app.js)

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react' ;
+
+const Header = () => {
+  return (
+    <div className='logo'>
+        <img src="/img/dc-logo.png" alt="dc-logo" />
+    </div>
+  )
+
+
+}
+
+export default Header
+
