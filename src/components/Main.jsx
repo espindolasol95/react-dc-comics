3ede656e9a72a55fff6b881ea4fc4ea984e9059b
espindolasol95 <espindolasol.ok@gmail.com>
2025-08-26 00:49:36 +0200

    feat(composizione  e stilizzazione main)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 
 const Main = () => {
   return (
+    <>
      <main className="hero">
       <p> -- Content goes here --</p>
     </main>
+
+    <section className='blue-banner'>
+      <div className='item'>
+        <img src="/img/buy-comics-digital-comics.png" alt="" /> 
+        <p>DIGITAL COMICS</p>
+      </div>
+      <div className='item'>
+        <img src="/img/buy-comics-merchandise.png" alt="" /> 
+        <p>DC MERCHANDISE</p>
+      </div>
+      <div className='item'>
+        <img src="/img/buy-comics-subscriptions.png" alt="" /> 
+        <p>SUBSCRIPTION</p>
+      </div>
+      <div className='item'>
+        <img src="/img/buy-comics-shop-locator.png" alt="" /> 
+        <p>COMIC SHOP LOCATOR</p>
+      </div>
+      <div className='item'>
+        <img src="/img/buy-dc-power-visa.svg" alt="" /> 
+        <p>DC POWER VISA </p>
+      </div>
+    </section>
+    </>
   )
 }
 
