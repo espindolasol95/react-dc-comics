63fd7c339c7e4af2f3300114501680fa76d9c42e
espindolasol95 <espindolasol.ok@gmail.com>
2025-08-26 17:22:10 +0200

    feat(stilizzazione delle colonne )

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,66 +4,65 @@ const Footer = () => {
   return (
   
     <footer>
-        <div className='dc-footer'>
-            <div className='contenitore'>
-                <div className='row'>
-                    <div className='col'>
-                        <h4>DC COMICS</h4>
-                        <ul>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Comics</a></li>
-                            <li><a href="#">Movies</a></li>
-                            <li><a href="#">TV</a></li>
-                            <li><a href="#">Games</a></li>
-                            <li><a href="#">Videos</a></li>
-                            <li><a href="#">News</a></li>
+  <div className='dc-footer'>
+    <div className='contenitore'>
+      <div className='row'>
 
-                        </ul>
-                        <h4>SHOP</h4>
-                        <ul>
-                            <li><a href="#">Shop DC</a></li>
-                            <li><a href="#">Shop DC collectibles</a></li>
+       
+        <div className='col'>
+          <h4>DC COMICS</h4>
+          <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
 
-                        </ul>
-                    <div className="col">
-                         <h4>DC</h4>
-                        <ul>
-                          <li><a href="#">Terms Of Use</a></li>
-                          <li><a href="#">Privacy policy (New)</a></li>
-                          <li><a href="#">Ad Choices</a></li>
-                          <li><a href="#">Advertising</a></li>
-                          <li><a href="#">Jobs</a></li>
-                          <li><a href="#">Subscription</a></li>
-                          <li><a href="#">Talent Workshops</a></li>
-                          <li><a href="#">CPSC Certificate</a></li>
-                          <li><a href="#">Ratings</a></li>
-                          <li><a href="#">Shop Help</a></li>
-                          <li><a href="#">Contac Us</a></li>
-                        </ul>
-                    </div>
-
-                    <div className='col'>
-                        <h4>SITES </h4>
-                        <ul>
-                            <li><a href="#">DC</a></li>
-                            <li><a href="#">mad Magazine</a></li>
-                            <li><a href="#">DC Kids</a></li>
-                            <li><a href="#">DC Universe</a></li>
-                            <li><a href="#">DC Power Visa</a></li>
-                        </ul>
-                    </div>
-
-                   <div className=' col dc-logo'></div>
-                   <img src="" alt="" />
-
-
-                    </div>
-
-                </div>
-
-            </div>
+          <h4>SHOP</h4>
+          <ul>
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>
+          </ul>
         </div>
-    </footer>
+
+        
+        <div className="col">
+          <h4>DC</h4>
+          <ul>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Privacy Policy (New)</a></li>
+            <li><a href="#">Ad Choices</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscription</a></li>
+            <li><a href="#">Talent Workshops</a></li>
+            <li><a href="#">CPSC Certificate</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Shop Help</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+
+       
+        <div className='col'>
+          <h4>SITES</h4>
+          <ul>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazine</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">DC Universe</a></li>
+            <li><a href="#">DC Power Visa</a></li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</footer>
+
    
   )
 }
