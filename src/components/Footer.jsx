b99a0f2086b5bde3da8552686ba99c9bf94793a7
espindolasol95 <espindolasol.ok@gmail.com>
2025-08-26 15:52:35 +0200

    feat(prima e seconda colonna del fotter)

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+  
+    <footer>
+        <div className='dc-footer'>
+            <div className='contenitore'>
+                <div className='row'>
+                    <div className='col'>
+                        <h4>dc comics</h4>
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </footer>
+   
+  )
+}
+
+export default Footer
