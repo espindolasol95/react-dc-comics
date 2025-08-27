c31a18e1d0ce6ebd305faca89e51fc2162ea1b3a
espindolasol95 <espindolasol.ok@gmail.com>
2025-08-28 01:22:30 +0200

    feat(creazione degli array e struttura della nav)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,59 @@
 import React from 'react' ;
 
 const Header = () => {
+  const nav = [
+  { id: 1,
+    label: "characters",
+    active: false,
+    href: "#"
+  },
+  { id: 2,
+    label: "comics",
+    active: false,
+    href: "#"
+  },
+  { id: 3,
+    label: "movies",
+    active: false,
+    href: "#"
+  },
+  { id: 4,
+    label: "tv",
+    active: false,
+    href: "#"
+  },
+  { id: 5,
+    label: "games",
+    active: false,
+    href: "#"
+  },
+  { id: 6,
+    label: "collectibles",
+    active: false, 
+    href: "#"
+  },
+  { id: 7,
+    label: "videos",
+    active: false,
+    href: "#"
+  },
+  { id: 8,
+    label: "fans",
+    active: false,
+    href: "#"
+  },
+  { id: 9,
+    label: "news",
+    active: false,
+    href: "#"
+  },
+  { id: 10,
+    label: "shop",
+    active: false,
+    href: "#"
+  },
+
+  ]
   return (
     <header className='header-col'>
     <div className='logo'>
@@ -8,16 +61,13 @@ const Header = () => {
     </div>
      <nav className="nav">
         <ul>
-          <li><a href="#">characters</a></li>
-          <li><a href="#" className="active">comics</a></li>
-          <li><a href="#">movies</a></li>
-          <li><a href="#">tv</a></li>
-          <li><a href="#">games</a></li>
-          <li><a href="#">collectibles</a></li>
-          <li><a href="#">videos</a></li>
-          <li><a href="#">fans</a></li>
-          <li><a href="#">news</a></li>
-          <li><a href="#">shop</a></li>
+          {nav.map((link)=>{
+            const{id,href,label,active}=link;
+            return(
+              <li key ={id} className={active === true ? 'active':''}>
+                <a href={href}>{label}</a></li>
+            )
+          })};
         </ul>
       </nav>
     </header>
