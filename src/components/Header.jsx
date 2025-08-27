621255521bb0a7cfc65d07cfb4e5b9f0e134c5fd
espindolasol95 <espindolasol.ok@gmail.com>
2025-08-28 00:55:35 +0200

    test(modifiche di preferenze)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,20 +6,20 @@ const Header = () => {
     <div className='logo'>
         <img src="/img/dc-logo.png" alt="dc-logo" />
     </div>
-    <nav className='nav'>
-      <ul>
-       <li>characters</li>
-        <li className='active'>comics</li>
-        <li>movies</li>
-        <li>tv</li>
-        <li>games</li>
-        <li>games</li>
-        <li>videos</li>
-        <li>fans</li>
-        <li>news</li>
-        <li>shop</li>
-      </ul>
-    </nav>
+     <nav className="nav">
+        <ul>
+          <li><a href="#">characters</a></li>
+          <li><a href="#" className="active">comics</a></li>
+          <li><a href="#">movies</a></li>
+          <li><a href="#">tv</a></li>
+          <li><a href="#">games</a></li>
+          <li><a href="#">collectibles</a></li>
+          <li><a href="#">videos</a></li>
+          <li><a href="#">fans</a></li>
+          <li><a href="#">news</a></li>
+          <li><a href="#">shop</a></li>
+        </ul>
+      </nav>
     </header>
   )
 
